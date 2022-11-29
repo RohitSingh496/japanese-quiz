cb2c1bd73fb57f710dc431b2d1bba98fb15e7145
Rohit <sawn@pop-os>
2022-11-29 21:55:04 +0530

    more_tasks_done

--- quiz.sh
+++ quiz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 echo "Creating a quiz from kanji.tsv file..."
 
 break(){
@@ -16,10 +16,20 @@ str=$(shuf -n 1 kanji.tsv)
 
 #extractWord
 word=$(echo "$str" | cut -f 2)
+
 #extractMeaning
-ans=$(echo "$str" | cut -f5)
+ans=$(echo "$str" | cut -f 5)
+
+#createRandomChoice
+choice1=$(shuf -n 1 kanji.tsv | cut -f 5 )
+choice2=$(shuf -n 1 kanji.tsv | cut -f 5 )
+choice3=$(shuf -n 1 kanji.tsv | cut -f 5 )
+
 
 echo "$word	$ans"
-
+break
+echo "Press Enter to see options..."
+read
+reply=$(echo -e "$ans\n$choice1\n$choice2\n$choice3" | fzf --prompt "Options for this: `echo $word`")
 
 break
