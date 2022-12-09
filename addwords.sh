e9c6ec0cd90551b729bd7ee2650a960fd956e27e
Rohit <sawn@pop-os>
2022-12-09 22:40:27 +0530

    Added wordcheck in addwords.sh

--- addwords.sh
+++ addwords.sh
@@ -6,5 +6,15 @@ read word
 echo "Add meaning: "
 read meaning
 
-echo "$word\t$meaning" >> ./allWords.tsv
-echo "$word\t$meaning" >> ./n4kanji.tsv
+list1check=`cut -f1 ./allWords.tsv | grep -Fxq "$word"; echo $?`
+list2check=`cut -f1 ./n4kanji.tsv | grep -Fxq "$word"; echo $?`
+
+
+if [ $list1check -eq 0 -o $list2check -eq 0 ];
+then
+    echo "\nThis word already exists!\n\n-----*****-----\n"
+    ./addwords.sh
+else
+    echo "$word\t$meaning" >> ./allWords.tsv
+    echo "$word\t$meaning" >> ./n4kanji.tsv
+fi
